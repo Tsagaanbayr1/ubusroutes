05b4dee87e8225f42a210f8472329c1226192709
Habiasa Masaka <tsagaanbayr.m@gmail.com>
2021-04-18 03:07:38 +0800

    Merge fixed from development side

--- src/db/db-connection.js
+++ src/db/db-connection.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const dotenv = require('dotenv');
 dotenv.config();
 const mysql2 = require('mysql2');
@@ -61,72 +60,3 @@ const HttpStatusCodes = Object.freeze({
     ER_TRUNCATED_WRONG_VALUE_FOR_FIELD: 422,
     ER_DUP_ENTRY: 409
 });
-
-
-=======
-const dotenv = require('dotenv');
-dotenv.config();
-const mysql2 = require('mysql2');
-
-class DBConnection {
-    constructor() {
-        this.db = mysql2.createPool({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASS,
-            database: process.env.DB_DATABASE
-        });
-
-        this.checkConnection();
-    }
-
-    checkConnection() {
-        this.db.getConnection((err, connection) => {
-            if (err) {
-                if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-                    console.error('Database connection was closed.');
-                }
-                if (err.code === 'ER_CON_COUNT_ERROR') {
-                    console.error('Database has too many connections.');
-                }
-                if (err.code === 'ECONNREFUSED') {
-                    console.error('Database connection was refused.');
-                }
-            }
-            if (connection) {
-                connection.release();
-            }
-            return
-        });
-    }
-
-    async query(sql, values) {
-        return new Promise((resolve, reject) => {
-            const callback = (error, result) => {
-                if (error) {
-                    reject(error);
-                    return;
-                }
-                resolve(result);
-            }
-            // execute will internally call prepare and query
-            this.db.execute(sql, values, callback);
-        }).catch(err => {
-            const mysqlErrorList = Object.keys(HttpStatusCodes);
-            // convert mysql errors which in the mysqlErrorList list to http status code
-            err.status = mysqlErrorList.includes(err.code) ? HttpStatusCodes[err.code] : err.status;
-
-            throw err;
-        });
-    }
-}
-
-// like ENUM
-const HttpStatusCodes = Object.freeze({
-    ER_TRUNCATED_WRONG_VALUE_FOR_FIELD: 422,
-    ER_DUP_ENTRY: 409
-});
-
-
->>>>>>> 9d5afe21741e7f4a1bc74b12b2afe87525af79a9
-module.exports = new DBConnection().query;
